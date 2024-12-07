1f7ced2ef098d5639a25c2cedb2f593593d99a76
Amal Our <ourajimamal@gmail.com>
2024-12-07 10:21:39 +0100

    v2

---
+++ src/main/java/ma/projet/grpc/entities/Compte.java
@@ -0,0 +1,18 @@
+package ma.projet.grpc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private float solde;
+    private String dateCreation;
+    private String type;
+}
